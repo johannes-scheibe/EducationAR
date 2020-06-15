34259910fa909f6efa362b6b18ee54488359074d
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-06-15 13:32:15 +0200

    First simple Ar-App

---
+++ app/src/main/java/com/example/educationar/EducationARRenderer.java
@@ -0,0 +1,73 @@
+package com.example.educationar;
+
+import org.artoolkitx.arx.arxj.ARController;
+import org.artoolkitx.arx.arxj.Trackable;
+import org.artoolkitx.arx.arxj.rendering.ARRenderer;
+import org.artoolkitx.arx.arxj.rendering.shader_impl.Cube;
+import org.artoolkitx.arx.arxj.rendering.shader_impl.SimpleFragmentShader;
+import org.artoolkitx.arx.arxj.rendering.shader_impl.SimpleShaderProgram;
+import org.artoolkitx.arx.arxj.rendering.shader_impl.SimpleVertexShader;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+import android.opengl.GLES20;
+
+public class EducationARRenderer extends ARRenderer {
+    private SimpleShaderProgram shaderProgram;
+
+    //TODO: I think we should add the trackable class to the library (arxj)
+
+    private static final Trackable trackables[] = new Trackable[]{
+            new Trackable("hiro", 80.0f),
+            new Trackable("kanji", 80.0f)
+    };
+    private int trackableUIDs[] = new int[trackables.length];
+
+    private Cube cube;
+
+    /**
+     * Markers can be configured here.
+     */
+    @Override
+    public boolean configureARScene() {
+        int i = 0;
+        for (Trackable trackable : trackables) {
+            trackableUIDs[i] = ARController.getInstance().addTrackable("single;Data/" + trackable.getName() + ".patt;" + trackable.getWidth());
+            if (trackableUIDs[i] < 0) return false;
+            i++;
+        }
+        return true;
+    }
+
+    //Shader calls should be within a GL thread. GL threads are onSurfaceChanged(), onSurfaceCreated() or onDrawFrame()
+    //As the cube instantiates the shader during setShaderProgram call we need to create the cube here.
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        this.shaderProgram = new SimpleShaderProgram(new SimpleVertexShader(), new SimpleFragmentShader());
+        cube = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
+        cube.setShaderProgram(shaderProgram);
+        super.onSurfaceCreated(unused, config);
+    }
+
+    /**
+     * Override the draw function from ARRenderer.
+     */
+    @Override
+    public void draw() {
+        super.draw();
+
+        GLES20.glEnable(GLES20.GL_CULL_FACE);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glFrontFace(GLES20.GL_CCW);
+
+        // Look for trackables, and draw on each found one.
+        for (int trackableUID : trackableUIDs) {
+            // If the trackable is visible, apply its transformation, and render a cube
+            float[] modelViewMatrix = new float[16];
+            if (ARController.getInstance().queryTrackableVisibilityAndTransformation(trackableUID, modelViewMatrix)) {
+                float[] projectionMatrix = ARController.getInstance().getProjectionMatrix(10.0f, 10000.0f);
+                cube.draw(projectionMatrix, modelViewMatrix);
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/educationar/MainActivity.java
+++ app/src/main/java/com/example/educationar/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.educationar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import org.artoolkitx.arx.arxj.ARActivity;
+import org.artoolkitx.arx.arxj.assets.AssetHelper;
+import org.artoolkitx.arx.arxj.rendering.ARRenderer;
+
+public class MainActivity extends ARActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initializeInstance();
+    }
+
+    /**
+     *  Passes the renderer used to create the ar content to artoolkitX
+     */
+    @Override
+    protected ARRenderer supplyRenderer() {
+        return new EducationARRenderer();
+    }
+
+    /**
+     * Use the FrameLayout in this Activity's UI.
+     */
+    @Override
+    protected FrameLayout supplyFrameLayout() {
+        return (FrameLayout) this.findViewById(R.id.mainFrameLayout);
+    }
+
+    // Here we do one-off initialisation which should apply to all activities
+    // in the application.
+    protected void initializeInstance() {
+
+        // Unpack assets to cache directory so native library can read them.
+        // N.B.: If contents of assets folder changes, be sure to increment the
+        // versionCode integer in the modules build.gradle file.
+        AssetHelper assetHelper = new AssetHelper(getAssets());
+        assetHelper.cacheAssetFolder(this, "Data");
+        assetHelper.cacheAssetFolder(this, "cparam_cache");
     }
 }
