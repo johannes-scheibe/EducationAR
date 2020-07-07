99fa90601d3ba8920b5210c49a0c3ccc68d903e2
Johannes Scheibe <johannes.scheibe@uni-oldenburg.de>
2020-07-07 15:49:40 +0200

    Added ObjLoader and TextureLoader

---
+++ EducationAR/app/src/main/java/com/example/educationar/utils/ObjLoader.java
@@ -0,0 +1,140 @@
+package com.example.educationar.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.util.Log;
+
+import com.example.educationar.MainActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class ObjLoader {
+
+    private static Logger logger = Logger.getLogger("EduAR-ObjLoader");
+
+    public float vertices[];
+    public float normals[];
+    public float textures[];
+    public short vertexIndices[];
+    public short textureIndices[];
+    public short normalIndices[];
+
+
+    public ObjLoader(Context context, String file) {
+
+        int numFaces;
+        List<Float> vertices = new ArrayList<Float>();
+        List<Float> normals = new ArrayList<Float>();
+        List<Float> textures = new ArrayList<Float>();
+
+        List<String> faces = new ArrayList<>();
+
+        BufferedReader reader = null;
+
+        try {
+            InputStreamReader in = new InputStreamReader(context.getAssets().open(file));
+            reader = new BufferedReader(in);
+
+            // read file until EOF
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                switch (parts[0]) {
+                    case "v":
+                        // vertices
+                        vertices.add(Float.valueOf(parts[1]).floatValue()); // Opengl x-Coordinate = Blender x-Coordinate
+                        vertices.add(-Float.valueOf(parts[3]).floatValue()); //Opengl y-Coordinate = - Blender z-Coordinate
+                        vertices.add(Float.valueOf(parts[2]).floatValue()); // Opengl z-Coordinate = Blender y-Coordinate
+                        break;
+                    case "vn":
+                        // normals
+                        normals.add(Float.valueOf(parts[1]).floatValue());
+                        normals.add(-Float.valueOf(parts[3]).floatValue());
+                        normals.add(Float.valueOf(parts[2]).floatValue());
+                        break;
+                    case "vt":
+                        // textures
+                        textures.add(Float.valueOf(parts[1]).floatValue());
+                        textures.add(Float.valueOf(parts[2]).floatValue());
+                        break;
+                    case "f":
+                        // faces: vertex/texture/normal
+                        faces.add(parts[1]);
+                        faces.add(parts[2]);
+                        faces.add(parts[3]);
+                        break;
+
+                }
+            }
+
+
+            // Create indices
+            vertexIndices = new short[faces.size()];
+            textureIndices = new short[faces.size()];
+            normalIndices = new short[faces.size()];
+            for (int i = 0; i < faces.size(); i++) {
+                String[] parts = faces.get(i).split("/");
+                logger.log(Level.INFO, faces.get(i));
+                // Vertex index
+                short vIndex = Short.valueOf(parts[0]).shortValue();
+                vertexIndices[i] = (vIndex -= 1);
+                logger.log(Level.INFO,""+vIndex);
+                // Texture index
+                short tIndex = Short.valueOf(parts[1]).shortValue();
+                textureIndices[i] = (tIndex -= 1);
+                // Normal index
+                short nIndex = Short.valueOf(parts[2]).shortValue();
+                normalIndices[i] = (nIndex -= 1);
+            }
+
+            // Convert ArrayLists to arrays
+            int i;
+            float[] vArray = new float[vertices.size()];
+            i = 0;
+            for (Float value : vertices) {
+                vArray[i++] = value;
+            }
+            this.vertices = vArray;
+
+            float[] tArray = new float[textureIndices.length*2];
+            i = 0;
+            for (Short index : textureIndices) {
+                tArray[i++] = textures.get(index*2);
+                tArray[i++] = textures.get(index*2+1);
+            }
+
+            this.textures = tArray;
+
+            float[] nArray = new float[normalIndices.length*3];
+            i = 0;
+            for (Short index : normalIndices) {
+                nArray[i++] = normals.get(index*3);
+                nArray[i++] = normals.get(index*3+1);
+                nArray[i++] = normals.get(index*3+2);
+            }
+            this.normals = nArray;
+
+
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+    }
+}
---
+++ EducationAR/app/src/main/java/com/example/educationar/utils/TextureLoader.java
@@ -0,0 +1,53 @@
+package com.example.educationar.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TextureLoader {
+
+    private static Logger logger = Logger.getLogger("EduAR-TextureLoader");
+
+    public static int loadTexture(final Context context, final String file)
+    {
+        final int[] textureHandle = new int[1];
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+
+        try{
+            if (textureHandle[0] == 0)
+            {
+                throw new RuntimeException("Error generating texture name.");
+            }
+
+            // Read in the resource
+            final Bitmap bitmap = BitmapFactory.decodeStream(context.getAssets().open(file));
+            logger.log(Level.INFO, "" + file);
+
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+            // Recycle the bitmap, since its data has been loaded into OpenGL.
+            bitmap.recycle();
+        }catch(IOException e){
+            logger.log(Level.WARNING, "Texture loading failed due a IO Exception:" + e.getMessage());
+        }
+
+        logger.log(Level.INFO, "Texture loading finished:" + textureHandle[0]);
+        return textureHandle[0];
+
+    }
+}
